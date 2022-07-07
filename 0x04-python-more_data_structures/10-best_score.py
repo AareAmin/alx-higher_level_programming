69710367147cc9a52089b74e7815baf61d255a7d
AareAmin <aarelatosa@gmail.com>
2022-07-07 00:56:09 -0700

    task update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    result = 0
-    max = ""
-    if a_dictionary:
-        for key, value in a_dictionary.items():
-            if value > result:
-                max = key
-                max = value
-        return max
-    else:
-        return none
+    v = list(a_dictionary.values())
+    k = list(a_dictionary.keys())
+              return k[v.index(max(v))]
