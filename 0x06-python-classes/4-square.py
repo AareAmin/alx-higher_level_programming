71b160eb751ce9353269810bf89de34c767afdd3
AareAmin <aarelatosa@gmail.com>
2022-07-19 13:55:26 -0700

    python-classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Defines a square-printing function."""
+class Square:
+    def __init__(self, size=0):
+        self.size = size
 
+    @property
+    def size(self):
+        return self.__size
 
-def print_square(size):
-    """Print a square with the # character.
-    Args:
-        size (int): The height/width of the square.
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    def area(self):
+        a = self.__size * self.__size
+        return a
