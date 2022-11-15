a3d4a4380c9a449e4d62541dba124cb38165d95a
AareAmin <aarelatosa@gmail.com>
2022-11-15 11:53:49 -0800

    js web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,34 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-/*
-let characters = [];
-let dict = {};
-
-function addToDict (url, name) {
-  dict[url] = name;
-}
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  characters = JSON.parse(body).characters
-  characters.forEach(function (url) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      addToDict(url, JSON.parse(body).name);
-    });
-  });
-  characters.forEach(function (item) {
-    console.log(dict[item]);
-  })
-});
-*/
-
 function helpRequest (arr, i) {
   if (i === arr.length) {
     return;
@@ -42,10 +14,13 @@ function helpRequest (arr, i) {
   });
 }
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
+request(
+  'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    const charac = JSON.parse(body).characters;
+    helpRequest(charac, 0);
   }
-  const charac = JSON.parse(body).characters;
-  helpRequest(charac, 0);
-});
+);
